af32329a30d33d421fee8991910e18c9e9405467
trandaine <tranquangdai03072004@outlook.com.vn>
2026-07-27 23:46:12 +0700

    update personal card v0.1

--- HTMLCSSFunamentals/PersonalCard/index.js
+++ HTMLCSSFunamentals/PersonalCard/index.js
@@ -5,7 +5,7 @@
 favouriteMovieGenre("military")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("blueberry")
+favouriteFruit("tomato")
 
 // light, dark
 favouriteMode("light")
