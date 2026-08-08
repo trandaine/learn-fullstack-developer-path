5a2604fa28ad399b1677ec886ecdad628ba60f55
trandaine <tranquangdai03072004@outlook.com.vn>
2026-08-08 23:03:04 +0700

    create a swappable button p1

--- EssentialJavaScript/JobArticle/index.js
+++ EssentialJavaScript/JobArticle/index.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+
 
 setTimeout(function () {
   modal.style.display = 'inline'
@@ -9,6 +11,11 @@ setTimeout(function () {
 
 modalCloseBtn.addEventListener('click', function () {
   modal.style.display = 'none'
+})
+
+
+declineBtn.addEventListener('mouseenter', function(){
+    console.log('hovered')
 })
 
 consentForm.addEventListener('submit', function (e) {
