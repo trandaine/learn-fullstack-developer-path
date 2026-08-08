efd6fa6693127a4b3783b133ee6a25f376b387d0
trandaine <tranquangdai03072004@outlook.com.vn>
2026-08-08 16:44:47 +0700

    make the modal reappear

--- EssentialJavaScript/JobArticle/index.js
+++ EssentialJavaScript/JobArticle/index.js
@@ -1,9 +1,13 @@
 /*
 Challenge:
-1. Log out the phrase "Modal Opened!" 
-   after a 1.5 second delay.
+1. Take control of the 'modal' div with JavaScript.
+2. Swap out our console.log for a line of code
+   which will change the CSS 'display' property
+   of our modal to 'inline'.
 */
 
-setTimeout(() => {
-  console.log("Modal Opened!");
-}, 1500);
+const modal = document.getElementById('modal')
+
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
