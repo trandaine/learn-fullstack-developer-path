05db2b3408b982a39f6209842a19e7f4be46ed88
trandaine <tranquangdai03072004@outlook.com.vn>
2026-08-15 01:24:33 +0700

    learning: connect with supabase and write table with embeddings generated

--- AI-Learning/Vector-Database/config.js
+++ AI-Learning/Vector-Database/config.js
@@ -1,16 +1,19 @@
 import OpenAI from 'openai';
 import { createClient } from "@supabase/supabase-js";
 
+// console.log("Env variables loaded:", Boolean(import.meta.env.OPENAI_API_KEY), Boolean(import.meta.env.SUPABASE_API_KEY), Boolean(import.meta.env.SUPABASE_URL));
+
 /** OpenAI config */
-if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!import.meta.env.VITE_OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  baseURL: import.meta.env.VITE_AI_URL,
   dangerouslyAllowBrowser: true
 });
 
 /** Supabase config */
-const privateKey = process.env.SUPABASE_API_KEY;
-if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
-const url = process.env.SUPABASE_URL;
-if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+const privateKey = import.meta.env.VITE_SUPABASE_API_KEY;
+if (!privateKey) throw new Error(`Expected env var VITE_SUPABASE_API_KEY`);
+const url = import.meta.env.VITE_SUPABASE_URL;
+if (!url) throw new Error(`Expected env var VITE_SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
--- AI-Learning/Vector-Database/index.js
+++ AI-Learning/Vector-Database/index.js
@@ -2,18 +2,22 @@ import { openai, supabase } from './config.js';
 import podcasts from './content.js';
 
 async function main(input) {
-  await Promise.all(
-    input.map( async (textChunk) => {
-        const embeddingResponse = await openai.embeddings.create({
-            model: "text-embedding-ada-002",
-            input: textChunk
-        });
-        const data = { 
-          content: textChunk, 
-          embedding: embeddingResponse.data[0].embedding 
-        }
-        console.log(data);  
-    })    
+  const data = await Promise.all(
+    input.map(async (textChunk) => {
+      const embeddingResponse = await openai.embeddings.create({
+        model: "openai/text-embedding-3-small",
+        input: textChunk
+      });
+      return {
+        content: textChunk,
+        embedding: embeddingResponse.data[0].embedding
+      }
+    })
   );
-  console.log('Embedding complete!');
+
+  // Insert content and embedding into Supabase
+  await supabase.from('documents').insert(data);
+  console.log('Embedding and storing complete!');
 }
+
+main(podcasts)
